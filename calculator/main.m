7d0738eeb9fe35f0952c819f705009d0e05df2e1
Mariah Cox <imariah77@gmail.com>
2013-12-03 16:15:23 -0800

    Initial commit

---
+++ calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calculator
+//
+//  Created by Mariah Cox on 12/3/13.
+//  Copyright (c) 2013 Mariah Cox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
